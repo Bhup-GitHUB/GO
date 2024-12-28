0a0ee871711596eb6aacd78181739def9499352a
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-12-28 11:16:49 +0530

    learning routing in golng

--- http/main.go
+++ http/main.go
@@ -10,7 +10,27 @@ type server struct{
 }
 
 func (s *server)ServeHTTP(w http.ResponseWriter,r *http.Request){
-	w.Write([]byte("Hi from the sever"))
+	switch r.Method{
+	case http.MethodGet:
+		switch r.URL.Path{
+			case "/" :
+				w.Write([]byte("index page"))
+				return
+			case "/user":
+				w.Write([]byte("User page"))
+				return 
+
+			case "/main-page":
+				w.Write([]byte("Main page"))
+				return
+		}
+	default:
+		w.Write([]byte("Error 404"))
+		return
+		
+
+	}
+       
 }
 
 func main(){
